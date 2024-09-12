a0ef245f514cc445dc7be3259a28fa6c4b21ef6d
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-12 11:06:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+  
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+
+void sortnumber(stack<int>&st,int temp){
+    if(st.empty()||st.top()<temp){
+        st.push(temp);
+    }
+    else{
+        int element=st.top();
+        st.pop();
+        sortnumber(st,temp);
+        st.push(element);
+    }
+}
+void SortedStack :: sort()
+{
+   //Your code here
+   if(!s.empty()){
+       int temp=s.top();
+       s.pop();
+       sort();
+       sortnumber(s,temp);
+   }
+}
