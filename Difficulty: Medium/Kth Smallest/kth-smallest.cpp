f36932f597410a9e351c689346ba706944c2bd17
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-19 19:55:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -17,9 +17,10 @@ class Solution {
         priority_queue<int> pq;
         for(int i=0;i<k;i++){
             pq.push(arr[i]);
+            
         }
         for(int i=k;i<arr.size();i++){
-            if(arr[i]<pq.top()){
+            if(pq.top()>arr[i]){
                 pq.pop();
                 pq.push(arr[i]);
             }
