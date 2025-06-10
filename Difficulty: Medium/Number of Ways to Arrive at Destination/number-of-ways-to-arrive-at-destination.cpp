2361795175c6def22bc29499355c13e3f7973585
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-06-10 21:47:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Ways to Arrive at Destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,46 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        // code here
+        vector<vector<pair<int, int>>> adj(n);
+        for(int i=0;i<roads.size();i++){
+            int a=roads[i][0];
+            int b=roads[i][1];
+            int c=roads[i][2];
+            adj[a].push_back({b,c});
+            adj[b].push_back({a, c});
+        }
+        int mod = 1e9 + 7;
+        vector<long long> dist(n,LLONG_MAX);
+        dist[0]=0;
+        int ans=1;
+        vector<long long> ways(n,0);
+        ways[0]=1;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>>pq;
+        pq.push({0,0});
+        while(!pq.empty()){
+            long long node=pq.top().second;
+            long long d=pq.top().first;
+            pq.pop();
+            
+            for(int i=0;i<adj[node].size();i++){
+                int nnode=adj[node][i].first;
+                long long dd=adj[node][i].second;
+                long long newdist=d+dd;
+                if(newdist<=dist[nnode]){
+                    if(newdist==dist[nnode]){
+                        ways[nnode]=(ways[nnode]+ways[node])%mod;
+                        continue;
+                    }
+                    ways[nnode]=ways[node];
+                    pq.push({newdist,nnode});
+                    dist[nnode]=newdist;
+                    
+                }
+            }
+        }
+        return ways[n-1];
+    }
+};
